9557fe3d46f0cd14817855866328f3ab966b49a3
I Am Kio <kieran@digitalindustria.com>
2024-03-16 13:02:49 +0000

    Design libraries and RTK stuff

---
+++ frontend/src/services/api.js
@@ -0,0 +1,44 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const tokenTipApi = createApi({
+  reducerPath: "tokenTipApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5001/token-tip-me/us-central1/api/",
+    prepareHeaders: async (headers) => {
+      const uid = localStorage.getItem("uid");
+
+      if (uid) {
+        headers.set("x-uid", uid);
+      } else {
+        console.warn("No UID found!");
+      }
+    },
+  }),
+  endpoints: (builder) => ({
+    getJars: builder.query({
+      query: () => `jars`,
+    }),
+    getAllJars: builder.query({
+      query: () => `jars/all`,
+    }),
+    getJarsById: builder.query({
+      query: (id) => `jars/handle/${id}`,
+    }),
+    updateJars: builder.mutation({
+      query: (createData) => ({
+        url: `jars`,
+        method: "POST",
+        body: createData,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetAllJarsQuery,
+  useGetJarsQuery,
+  useGetJarsByIdQuery,
+  useUpdateJarsMutation,
+} = tokenTipApi;
