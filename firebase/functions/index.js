c0298bfb508a835bcecf7225939eceba396391da
I Am Kio <kieran@digitalindustria.com>
2024-03-16 14:17:02 +0000

    Added firebase routes for DB reads

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,6 +1,9 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const express = require("express");
 
+const { read, add, readAll, readByHandle } = require("./controllers/jars");
+
+const { verifyAuth } = require("./middleware/auth");
 const { initFirebase } = require("./services/firebase");
 
 const api = express();
@@ -11,5 +14,10 @@ api.get("/", (req, res) =>
     message: "Hello from Token Tip @ ETH Global 🇬🇧 London Hackathon 2024!",
   })
 );
+
+api.get("/jars/all", readAll);
+api.get("/jars/handle/:handleId", readByHandle);
+api.get("/jars", verifyAuth, read);
+api.post("/jars", verifyAuth, add);
 
 exports.api = onRequest(api);
